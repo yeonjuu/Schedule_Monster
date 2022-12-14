6ba6e368647c015ef3328a338168fbb08890d43a
sangwooyu <dbtkddn598@gmail.com>
2022-12-14 23:36:22 +0900

    chore: ts관련 패키지 추가 설치 및 commonjs 타입 모듈 -> import문 변경

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,42 +1,42 @@
-import { NextFunction } from "express";
-import { errorHandler } from "./middleware/errorHandler";
-import createError from "http-errors";
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+import { NextFunction } from 'express';
+import { errorHandler } from './middleware/errorHandler';
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const logger = morgan;
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/userRouter');
 
 const app = express();
 
 /* eslint-disable no-console */
 
-const port = process.env.PORT || "3000";
+const port = process.env.PORT || '3000';
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use("*", function (req: Request, res: Response, next: NextFunction) {
-  next(createError(404));
-});
+// app.use('*', function (req: Request, res: Response, next: NextFunction) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(errorHandler);
 
 app.listen(port, () => {
-  // logger.info(`Server listening on port: ${port}`);
   console.log(`Server listening on port: ${port}`);
 });
