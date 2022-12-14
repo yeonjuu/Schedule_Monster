0f09bee04af5debd1739d906c6df0a2cf8bd4e2f
sangwooyu <dbtkddn598@gmail.com>
2022-12-14 21:28:27 +0900

    hotfix: typescript, ts-node 패키지 재설치 및 http-errors 설치

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,35 +1,35 @@
-import { NextFunction } from 'express';
-import { errorHandler } from './middleware/errorHandler';
-import createError from 'http-errors';
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+import { NextFunction } from "express";
+import { errorHandler } from "./middleware/errorHandler";
+import createError from "http-errors";
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
 /* eslint-disable no-console */
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || "3000";
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use('*', function (req: Request, res: Response, next: NextFunction) {
+app.use("*", function (req: Request, res: Response, next: NextFunction) {
   next(createError(404));
 });
 
@@ -37,5 +37,6 @@ app.use('*', function (req: Request, res: Response, next: NextFunction) {
 app.use(errorHandler);
 
 app.listen(port, () => {
-  logger.info(`Server listening on port: ${port}`);
+  // logger.info(`Server listening on port: ${port}`);
+  console.log(`Server listening on port: ${port}`);
 });
