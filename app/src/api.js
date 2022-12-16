05d0138c54f1edda2dcea3bbb2723e4d93549cd1
yeonjuu <yeonju9896@gmail.com>
2022-12-16 22:48:27 +0900

    fix: delete api 수정

--- app/src/api.js
+++ app/src/api.js
@@ -1,14 +1,9 @@
 import axios from 'axios';
 
-//api test json server
-//const jsonUrl = 'https://jsonplaceholder.typicode.com';
-
-//url endpoint/post 따로 받는게 편한지 아님 통으로 /topics/1 이게 편한지
-async function get(endpoint, params = '') {
-  const apiUrl = `${endpoint}/${params}`;
+async function get(endpoint) {
   //headers 값을 어떻게 넘겨줄지 . `Bearer ${sessionStorage.getItem('token')}` 이런식으로 ?
   try {
-    const result = await axios.get(apiUrl, {
+    const result = await axios.get(endpoint, {
       Authorization: `Basic token`,
     });
     //응답 값 전송
@@ -32,15 +27,11 @@ async function get(endpoint, params = '') {
   }
 }
 
-async function post(endpoint, params = '', data) {
-  const apiUrl = `${endpoint}/${params}`;
-
+async function post(endpoint, data) {
   try {
-    const response = await axios.post(apiUrl, data, {
+    const response = await axios.post(endpoint, data, {
       headers: { Authorization: `Bearer token` },
     });
-    //post된 data 리턴
-    //무턱대고 에러로 볼게 아니라 status 로 에러를 줘야하나?
     return response.data;
   } catch (error) {
     if (error.response) {
@@ -58,14 +49,11 @@ async function post(endpoint, params = '', data) {
 //put: 새로 데이터를 작성하는 것
 //patch : 덮어쓰기? 틀은 그대로 있고 안에 내용을 바꾸는거
 //기존 데이터 + 바뀌는 데이터 다 작성
-async function put(endpoint, params = '', data) {
-  const apiUrl = `${endpoint}/${params}`;
-
+async function put(endpoint, data) {
   try {
-    const response = await axios.put(apiUrl, data, {
+    const response = await axios.put(endpoint, data, {
       headers: { Authorization: `Bearer token` },
     });
-    //put으로 넣어진 값 전송
     return response.data;
   } catch (error) {
     if (error.response) {
@@ -81,14 +69,12 @@ async function put(endpoint, params = '', data) {
 }
 
 //변경할 데이터만 작성
-async function patch(endpoint, params = '', data) {
-  const apiUrl = `${endpoint}/${params}`;
-
+async function patch(endpoint, data) {
   try {
-    const response = await axios.patch(apiUrl, data, {
+    const response = await axios.patch(endpoint, data, {
       headers: { Authorization: `Bearer token` },
     });
-    //put으로 넣어진 값 전송
+
     return response.data;
   } catch (error) {
     if (error.response) {
@@ -102,15 +88,14 @@ async function patch(endpoint, params = '', data) {
     }
   }
 }
-//삭제할때 데이터가 필요해?
-async function del(endpoint, params = '') {
-  const apiUrl = `${endpoint}/${params}`;
 
+async function del(endpoint, data) {
   try {
-    const response = await axios.delete(apiUrl, {
+    const response = await axios.delete(endpoint, {
+      data: { data },
       headers: { Authorization: `Bearer token` },
     });
-    //성공시 빈배열 반환
+
     return response.data;
   } catch (error) {
     if (error.response) {
