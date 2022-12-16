4d8908ed7aaeca17d9c2def22b1329e0c5fc7913
HYK424 <turtle424@naver.com>
2022-12-16 15:50:15 +0900

    feat: App.tsx에 전역 스타일링 적용

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import GlobalStyle from './components/layout/globalStyle';
+import CalendarPage from './pages/CalenderPage';
 
 function App() {
   return (
-    <div className="App">
-      <p>화면</p>
-    </div>
+    <>
+      <GlobalStyle />
+      <div className="App">
+        <p>화면</p>
+      </div>
+    </>
   );
 }
 
