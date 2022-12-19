a6e7dedc3c69a34d4f147c942878d9ed842f155d
yeonjuu <yeonju9896@gmail.com>
2022-12-19 21:42:58 +0900

    fix: import수정

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,45 +1,40 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import GlobalStyle from './components/layout/globalStyle';
-import { LoginRegister } from './pages/Login/Login';
+import { LoginRegister } from './pages/login/Login';
 import Items from './pages/characters/Items';
 import CalendarPage from './pages/calendar/CalendarPage';
 import MyItems from './pages/characters/MyItems';
 import CharactersList from './pages/characters/CharactersList';
 import Root from './pages/characters/Root';
 
-
 const router = createBrowserRouter([
   {
-    path:'/',
-    element: <LoginRegister />
+    path: '/',
+    element: <LoginRegister />,
   },
   {
-    path:'/calendar',
-    element: <CalendarPage />
+    path: '/calendar',
+    element: <CalendarPage />,
   },
   {
-    path:'/store',
+    path: '/store',
     element: <Root />,
-    errorElement : <p>페이지를 찾을 수 없습니다😭</p>,
+    errorElement: <p>페이지를 찾을 수 없습니다😭</p>,
     children: [
-      {index : true, element : <Items></Items>},
-      {path : '/store/characters', element:<CharactersList></CharactersList>},
-      {path : '/store/myitems', element:<MyItems></MyItems>}
-    ]
-  }
+      { index: true, element: <Items></Items> },
+      { path: '/store/characters', element: <CharactersList></CharactersList> },
+      { path: '/store/myitems', element: <MyItems></MyItems> },
+    ],
+  },
 ]);
-
 
 function App() {
   return (
     <>
       <GlobalStyle />
-
       <RouterProvider router={router} />
-      <div className="App">
-      </div>
-      
+      <div className="App"></div>
     </>
   );
 }
