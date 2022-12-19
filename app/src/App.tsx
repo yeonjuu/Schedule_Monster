6c4daf098826d7c3e5c538be6fe7c5f95eada3ce
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2022-12-19 20:52:29 +0900

    feat : 라우터 연결 수정

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,27 +1,45 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, createBrowserRouter, RouterProvider} from 'react-router-dom';
 import GlobalStyle from './components/layout/globalStyle';
 import { LoginRegister } from './pages/Login/Login';
 import Items from './pages/characters/Items';
 import CalendarPage from './pages/calendar/CalendarPage';
 import MyItems from './pages/characters/MyItems';
 import CharactersList from './pages/characters/CharactersList';
+import Root from './pages/characters/Root';
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <LoginRegister />
+  },
+  {
+    path:'/calendar',
+    element: <CalendarPage />
+  },
+  {
+    path:'/store',
+    element: <Root />,
+    errorElement : <p>페이지를 찾을 수 없습니다😭</p>,
+    children: [
+      {index : true, element : <Items></Items>},
+      {path : '/store/characters', element:<CharactersList></CharactersList>},
+      {path : '/store/myitems', element:<MyItems></MyItems>}
+    ]
+  }
+]);
+
 
 function App() {
   return (
     <>
       <GlobalStyle />
+
+      <RouterProvider router={router} />
       <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LoginRegister />} />
-            <Route path="/store" element={<Items />} />
-            <Route path="/store/myitems" element={<MyItems />} />
-            <Route path="/store/characters" element={<CharactersList />} />
-            <Route path="/calendar" element={<CalendarPage />} />
-          </Routes>
-        </BrowserRouter>
       </div>
+      
     </>
   );
 }
