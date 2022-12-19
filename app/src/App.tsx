8241e5d7f6ae8ca3053d278fdd1fa3b8d3462431
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2022-12-19 23:12:37 +0900

    rename: routerprovider 위치 변경 (App -> Index)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,39 +1,13 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import GlobalStyle from './components/layout/globalStyle';
-import { LoginRegister } from './pages/login/Login';
-import Items from './pages/characters/Items';
-import CalendarPage from './pages/calendar/CalendarPage';
-import MyItems from './pages/characters/MyItems';
-import CharactersList from './pages/characters/CharactersList';
-import Root from './pages/characters/Root';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <LoginRegister />,
-  },
-  {
-    path: '/calendar',
-    element: <CalendarPage />,
-  },
-  {
-    path: '/store',
-    element: <Root />,
-    errorElement: <p>페이지를 찾을 수 없습니다😭</p>,
-    children: [
-      { index: true, element: <Items></Items> },
-      { path: '/store/characters', element: <CharactersList></CharactersList> },
-      { path: '/store/myitems', element: <MyItems></MyItems> },
-    ],
-  },
-]);
+
+
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <RouterProvider router={router} />
       <div className="App"></div>
     </>
   );
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,6 +4,35 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Items from './pages/characters/Items';
+import CalendarPage from './pages/calendar/CalendarPage';
+import MyItems from './pages/characters/MyItems';
+import CharactersList from './pages/characters/CharactersList';
+import Root from './pages/characters/Root';
+import { LoginRegister } from './pages/login/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginRegister />,
+  },
+  {
+    path: '/calendar',
+    element: <CalendarPage />,
+  },
+  {
+    path: '/store',
+    element: <Root />,
+    errorElement: <p>페이지를 찾을 수 없습니다😭</p>,
+    children: [
+      { index: true, element: <Items></Items> },
+      { path: '/store/characters', element: <CharactersList></CharactersList> },
+      { path: '/store/myitems', element: <MyItems></MyItems> },
+    ],
+  },
+]);
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -11,6 +40,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <RouterProvider router={router} />
       <App />
     </Provider>
     
