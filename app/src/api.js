66964a83c12ac7d08f24bf7b17c25ecea1a14026
yeonjuu <yeonju9896@gmail.com>
2022-12-20 14:44:44 +0900

    fix: api 권한 설정

--- app/src/api.js
+++ app/src/api.js
@@ -1,27 +1,23 @@
 import axios from 'axios';
 
 async function get(endpoint) {
-  //headers 값을 어떻게 넘겨줄지 . `Bearer ${sessionStorage.getItem('token')}` 이런식으로 ?
   try {
-    const result = await axios.get(endpoint, {
-      Authorization: `Basic token`,
+    const response = await axios.get(endpoint, {
+      Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
     });
-    //응답 값 전송
-    return result.data;
+    //응답 값 데이터만 전송
+    return response.data;
   } catch (error) {
     if (error.response) {
       // 요청이 전송되었고, 서버는 2xx 외의 상태 코드로 응답했습니다.
-      console.log('status code: ', error.response.status);
       throw new Error(error);
     } else if (error.request) {
       // 요청이 전송되었지만, 응답이 수신되지 않았습니다.
       // 'error.request'는 브라우저에서 XMLHtpRequest 인스턴스이고,
       // node.js에서는 http.ClientRequest 인스턴스입니다.
-      console.log('응답이 수신되지 않았습니다.');
       throw new Error(error.request);
     } else {
       // 오류가 발생한 요청을 설정하는 동안 문제가 발생했습니다.
-      console.log('요청 설정 에러', error.message);
       throw new Error(error.message);
     }
   }
@@ -30,18 +26,17 @@ async function get(endpoint) {
 async function post(endpoint, data) {
   try {
     const response = await axios.post(endpoint, data, {
-      headers: { Authorization: `Bearer token` },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
     });
     return response.data;
   } catch (error) {
     if (error.response) {
-      console.log('status code: ', error.response.status);
       throw new Error(error);
     } else if (error.request) {
-      console.log('응답이 수신되지 않았습니다.');
       throw new Error(error.request);
     } else {
-      console.log('요청 설정 에러', error.message);
       throw new Error(error.message);
     }
   }
@@ -52,17 +47,17 @@ async function post(endpoint, data) {
 async function put(endpoint, data) {
   try {
     const response = await axios.put(endpoint, data, {
-      headers: { Authorization: `Bearer token` },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
     });
     return response.data;
   } catch (error) {
     if (error.response) {
       throw new Error(error);
     } else if (error.request) {
-      console.log('응답이 수신되지 않았습니다.');
       throw new Error(error.request);
     } else {
-      console.log('요청 설정 에러', error.message);
       throw new Error(error.message);
     }
   }
@@ -72,7 +67,9 @@ async function put(endpoint, data) {
 async function patch(endpoint, data) {
   try {
     const response = await axios.patch(endpoint, data, {
-      headers: { Authorization: `Bearer token` },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
     });
 
     return response.data;
@@ -80,10 +77,8 @@ async function patch(endpoint, data) {
     if (error.response) {
       throw new Error(error);
     } else if (error.request) {
-      console.log('응답이 수신되지 않았습니다.');
       throw new Error(error.request);
     } else {
-      console.log('요청 설정 에러', error.message);
       throw new Error(error.message);
     }
   }
@@ -93,7 +88,9 @@ async function del(endpoint, data) {
   try {
     const response = await axios.delete(endpoint, {
       data: { data },
-      headers: { Authorization: `Bearer token` },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
     });
 
     return response.data;
@@ -101,10 +98,8 @@ async function del(endpoint, data) {
     if (error.response) {
       throw new Error(error);
     } else if (error.request) {
-      console.log('응답이 수신되지 않았습니다.');
       throw new Error(error.request);
     } else {
-      console.log('요청 설정 에러', error.message);
       throw new Error(error.message);
     }
   }
