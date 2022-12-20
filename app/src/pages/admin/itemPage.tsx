485d8203d4826c640f325973b950127193d417cd
odhdhfjf <odhdhfjf@gmail.com>
2022-12-21 04:32:01 +0900

    feat: css 수정

--- app/src/pages/admin/itemPage.tsx
+++ app/src/pages/admin/itemPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Container } from './bannerCss';
 import Search from '../../components/shop/search';
-import ItemList from '../../components/shop/ItemList';
+import ItemListComponents from '../../components/shop/ItemList';
 import BannerItem from '../../components/shop/categories';
 import EditItem from './editItem';
 import {
@@ -9,27 +9,30 @@ import {
   ItemContainer,
   CategoryBox,
   ItemBox,
+  ItemList,
 } from '../../components/characters/StoreStyle';
 import StoreItem from 'components/characters/StoreItem';
 function ItemPage({ categories, setCategory, category }: any) {
   const [inputState, setInputState] = useState('');
   return (
     <ContentsBox>
-      <ItemContainer>
-        <BannerItem
-          categories={categories}
-          setCategory={setCategory}
-          category={category}
-        ></BannerItem>
-        <Search setState={setInputState}></Search>
-        <CategoryBox>
-          <ItemList
-            category={category === 'all' ? 'all' : category}
-            inputValue={inputState}
-            url={'/admin/item/'}
-          ></ItemList>
-        </CategoryBox>
-      </ItemContainer>
+      <ItemList>
+        <ItemContainer>
+          <BannerItem
+            categories={categories}
+            setCategory={setCategory}
+            category={category}
+          ></BannerItem>
+          <Search setState={setInputState}></Search>
+          <CategoryBox>
+            <ItemListComponents
+              category={category === 'all' ? 'all' : category}
+              inputValue={inputState}
+              url={'/admin/item/'}
+            ></ItemListComponents>
+          </CategoryBox>
+        </ItemContainer>
+      </ItemList>
 
       <EditItem></EditItem>
     </ContentsBox>
