fac107533aef778e229fec8a463724b373bb5d6b
yeonjuu <yeonju9896@gmail.com>
2022-12-21 01:54:41 +0900

    fix: 상태 로컬저장

--- app/src/store/store.ts
+++ app/src/store/store.ts
@@ -1,9 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../pages/login/userSlice';
 import { items, monsterCategories, monsters, itemCategories } from './mockData';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
 const store = configureStore({
   reducer: {
-    userReducer,
+    persistedReducer,
     items,
     monsterCategories,
     itemCategories,
@@ -12,4 +22,5 @@ const store = configureStore({
 });
 
 export default store;
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
