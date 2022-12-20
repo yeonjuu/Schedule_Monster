3f781090dcd542d27234c681ccfa6b77d3731190
yeonjuu <yeonju9896@gmail.com>
2022-12-21 01:54:10 +0900

    feat: 네비게이션바 경로추가

--- app/src/components/navbar/NavBar.tsx
+++ app/src/components/navbar/NavBar.tsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../store/store';
 import { logout } from '../../pages/login/userSlice';
 import * as Nav from './nav';
+import { useNavigate } from 'react-router-dom';
 
 type IProps = {
   nickname: string;
@@ -11,13 +12,13 @@ type IProps = {
 };
 
 export const NavBar = () => {
-  const user = useSelector((state: RootState) => state.userReducer);
+  const user = useSelector((state: RootState) => state.persistedReducer);
   const dispatch = useDispatch();
-  console.log(user);
-  const { nickname, point } = user;
 
-  const clickLoginHandler = () => {
-    const isLogout = confirm('로그아웃하시겠습니까?');
+  const { nickname, point, isLogin } = user;
+
+  const clickLogoutHandler = () => {
+    const isLogout = window.confirm('로그아웃하시겠습니까?');
     if (isLogout) {
       dispatch(logout());
     }
@@ -26,33 +27,55 @@ export const NavBar = () => {
 
   return (
     <div>
-      {user.isLogin && (
+      {isLogin ? (
         <StateLogin
           nickname={nickname}
           point={point}
-          onClick={clickLoginHandler}
+          onClick={clickLogoutHandler}
         />
+      ) : (
+        <StateLogout />
       )}
-      {!user.isLogin && <StateLogout />}
     </div>
   );
 };
 
 const StateLogin = ({ nickname, point, onClick }: IProps) => {
+  const navigate = useNavigate();
+
   return (
     <Nav.TabWrapper>
-      <Nav.Tab>{nickname}님 마이페이지</Nav.Tab>
-      <Nav.Tab>캐릭터관리</Nav.Tab>
-      <Nav.Tab>💰{point}</Nav.Tab>
+      <Nav.Tab
+        onClick={() => {
+          navigate('/mypage');
+        }}
+      >
+        {nickname}님 마이페이지
+      </Nav.Tab>
+      <Nav.Tab
+        onClick={() => {
+          navigate('/store');
+        }}
+      >
+        캐릭터관리
+      </Nav.Tab>
+      <Nav.Tab nolink>💰{point}</Nav.Tab>
       <Nav.Tab onClick={onClick}>로그아웃</Nav.Tab>
     </Nav.TabWrapper>
   );
 };
 
 const StateLogout = () => {
+  const navigate = useNavigate();
   return (
     <Nav.TabWrapper>
-      <Nav.Tab>로그인</Nav.Tab>
+      <Nav.Tab
+        onClick={() => {
+          navigate('/login');
+        }}
+      >
+        로그인
+      </Nav.Tab>
     </Nav.TabWrapper>
   );
 };
