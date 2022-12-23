66310d0114f07223b56b2918ddffdc8cb2378338
yj0903 <wiseidea01@gmail.com>
2022-12-24 08:33:26 +0900

    feat: 사용자가 아이템 구매 로직/사용하는 로직

--- server/src/controller/userItemController.ts
+++ server/src/controller/userItemController.ts
@@ -6,6 +6,8 @@ interface userItemControllerInterface {
     getUserItem: AsyncRequestHandler;
     createUserItem: AsyncRequestHandler;
     deleteUserItem: AsyncRequestHandler;
+    buyUserItem: AsyncRequestHandler;
+    useUserItem: AsyncRequestHandler;
 }
 
 export const userItemController: userItemControllerInterface = {
@@ -25,6 +27,15 @@ export const userItemController: userItemControllerInterface = {
     async deleteUserItem(req, res) {
         const { id } = req.params;
         const response = await userItemService.deleteUserItem(id);
+        res.json(response);
+    },
+    async buyUserItem(req, res) {
+        const response = await userItemService.buyUserItem(req.body);
+        res.json(response);
+    },
+    async useUserItem(req, res) {
+        const { email, itemId, characterId } = req.body;
+        const response = await userItemService.useUserItem(email, itemId, characterId);
         res.json(response);
     },
 };
