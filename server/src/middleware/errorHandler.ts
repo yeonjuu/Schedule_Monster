9c4006c34bfb05fbb62e3686db082faa616a2a94
sangwooyu <dbtkddn598@gmail.com>
2022-12-23 21:51:50 +0900

    fix: Error 핸들러 미작동 수정

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -4,10 +4,16 @@ import { splitedError } from '../utils/splitedArr';
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   if (!err.message) {
     // app.ts파일의 404페이지 에러일 경우
+    console.log('app.ts에서 오류나는 상황');
     const { errType, errMessage } = splitedError(err);
     errorResponse(res, errType, errMessage);
   } else {
-    const { errType, errMessage } = splitedError(err.message);
-    errorResponse(res, errType, errMessage);
+    console.log('일반오류');
+    if (err.message.slice(0, 4) === 'type') {
+      const { errType, errMessage } = splitedError(err.message);
+      errorResponse(res, errType, errMessage);
+    } else {
+      errorResponse(res, 'BadRequest', err.message);
+    }
   }
 };
