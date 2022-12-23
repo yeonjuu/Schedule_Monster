d3fdeefc58e026e86d489e6edd7ef1b80f8a95fc
yeonjuu <yeonju9896@gmail.com>
2022-12-23 16:56:38 +0900

    style: 폰트 재설정

--- app/src/components/layout/globalStyle.tsx
+++ app/src/components/layout/globalStyle.tsx
@@ -4,7 +4,7 @@ const GlobalStyle = createGlobalStyle`
     *{
         margin: 0;
         padding: 0;
-        font-family : 'MICEGothic Bold', sans-serif;
+        font-family : 'IM_Hyemin-Bold', sans-serif;
     }
     @font-face {
         font-family: 'MICEGothic Bold';
@@ -13,24 +13,11 @@ const GlobalStyle = createGlobalStyle`
         font-style: normal;
     }
     @font-face {
-        font-family: 'NanumSquareNeo-Variable';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/NanumSquareNeo-Variable.woff2') format('woff2');
-        font-weight: normal;
-        font-style: normal;
-    }
-    @font-face {
         font-family: 'IM_Hyemin-Bold';
         src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2106@1.1/IM_Hyemin-Bold.woff2') format('woff');
         font-weight: normal;
         font-style: normal;
     }
-    @font-face {
-        font-family: 'HBIOS-SYS';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2207-01@1.0/HBIOS-SYS.woff2') format('woff2');
-        font-weight: normal;
-        font-style: normal;
-    }
-
 `;
 
 export default GlobalStyle;
