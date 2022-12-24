9715d4df1a341d7feb53bbb9620820680ac15144
yj0903 <wiseidea01@gmail.com>
2022-12-24 10:14:02 +0900

    fix: 헤더 에러 핸들링 재반영

--- server/src/utils/api.ts
+++ server/src/utils/api.ts
@@ -4,7 +4,8 @@ import { AsyncRequestHandler } from '../types';
 export const asyncHandler = (asyncHandlerArgFunc: AsyncRequestHandler) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      if (req.method !== 'GET' && req.headers['content-type'] !== 'application/json') {
+      // application/json으로 설정하지 않으면 body에 데이터가 담기지 않은 채로 요청됨
+      if ((req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH') && req.headers['content-type'] !== 'application/json') {
         throw new Error(
             "headers의 Content-Type을 application/json으로 설정해주세요"
         );
