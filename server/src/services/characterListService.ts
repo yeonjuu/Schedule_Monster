56f9596242d0fddb520abce203c2a0b73ed9654f
yj0903 <wiseidea1@hanmail.net>
2022-12-27 04:39:17 +0900

    fix: 불필요한 참조 삭제

--- server/src/services/characterListService.ts
+++ server/src/services/characterListService.ts
@@ -1,19 +1,16 @@
-import { characterListModel, characterListModelType } from '../models';
+import { userModel, characterListModel, characterListModelType } from '../models';
 import {CharacterListInterface, UpdateCharacterListInterface} from '../models/schemas/CharacterList';
-import { userModel, userModelType } from '../models';
 class CharacterListService {
   private characterList: characterListModelType;
-  private user: userModelType;
 
-  constructor(characterListModel: characterListModelType, userModel: userModelType) {
+  constructor(characterListModel: characterListModelType) {
     this.characterList = characterListModel;
-    this.user = userModel;
   }
 
   // 관리자용 : 전체 사용자 캐릭터 리스트 전체 조회
   async getCharacterLists(email: string) {
     console.log(email);
-    const user = await this.user.findOne({ email });
+    const user = await userModel.findOne({ email });
     if (!user) throw new Error('type:Forbidden,content:사용자가 존재하지 않거나 요청이 정상적이지 않습니다');
     if (user.auth === 'user') new Error('type:Forbidden,message:해당 관리 권한으로는 서비스 요청을 할 수 없습니다');
 
@@ -65,6 +62,6 @@ class CharacterListService {
     return result;
   }
 }
-const characterListService = new CharacterListService(characterListModel, userModel);
+const characterListService = new CharacterListService(characterListModel);
 
 export { characterListService };
