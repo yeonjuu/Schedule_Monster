814a5cabf1199f099634c0f7583ccd8413926b29
sangwooyu <dbtkddn598@gmail.com>
2022-12-26 23:31:53 +0900

    fix: http 상태코드 변경 403 -> 401

--- server/src/utils/errorResponse.ts
+++ server/src/utils/errorResponse.ts
@@ -2,7 +2,9 @@ import { Response } from 'express';
 
 export const errorResponse = (res: Response, type: string, message: string) => {
   let statusCode: number;
-  if (type == 'Forbidden') statusCode = 403;
+  // Forbidden 클라우드에서 자체적인 403에러처리를 진행함
+  // 의도한 오류가 호출이 안되어 401(unAuthorized code)로 변경
+  if (type == 'Forbidden') statusCode = 401;
   else if (type == 'NotFound') statusCode = 404;
   else if (type == 'BadRequest') statusCode = 400;
   else if (type == 'Internal Server Error') statusCode = 500;
