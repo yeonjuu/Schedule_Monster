1af237cf1496ef403ce08a3e8a9518a8f3ad7c17
odhdhfjf <odhdhfjf@gmail.com>
2022-12-29 05:42:28 +0900

    Fix: 안쓰는 컴포넌트로 인한 에러 수정

--- app/src/pages/admin/admin.tsx
+++ app/src/pages/admin/admin.tsx
@@ -4,14 +4,13 @@ import { Outlet } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 import { Header } from 'components/header/Header';
-import CharacterModal from 'components/characters/Charactermodal';
+
 function Admin() {
   const queryClient = new QueryClient();
   return (
     <>
       <Header></Header>
       <Banner></Banner>
-      <CharacterModal></CharacterModal>
 
       <QueryClientProvider client={queryClient}>
         <Outlet />
