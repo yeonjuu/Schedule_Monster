b5cbdd7dcbda5fba546ca4f85e2b301af3a1512f
yeonjuu <yeonju9896@gmail.com>
2022-12-29 18:12:07 +0900

    feat: 비회원 로그인창 리다이랙트

--- app/src/pages/characters/Root.tsx
+++ app/src/pages/characters/Root.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import Navbar from '../../components/characters/Navbar';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/store';
 
 const queryClient = new QueryClient();
 
 export default function Root() {
-    return (
+  const isLogin = useSelector(
+    (state: RootState) => state.persistedReducer.isLogin,
+  );
+  return (
+    <>
+      {isLogin ? (
         <>
-            <Navbar></Navbar>
-            <QueryClientProvider client={queryClient}>
-                <Outlet />
-            </QueryClientProvider>
+          <Navbar></Navbar>
+          <QueryClientProvider client={queryClient}>
+            <Outlet />
+          </QueryClientProvider>
         </>
-    );
+      ) : (
+        <Navigate to="/login" replace={true} />
+      )}
+    </>
+  );
 }
-
